8d30d57ed492479425607512b2ae12dec313ae6a
unknown <1033809259@qq.com>
2023-06-13 14:35:54 +0800

    OpenMMLab Camp

---
+++ MMPreTrain Tutorial/resnet50_finetune.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9c4fc46d58ae493987eeec7e42e457f43474561b78539300358954e0651d899f
+size 4480
---
+++ README.md
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b27a0825f8bc65ece5c9e6dfa6337c89d4472c095425f81bfac3eaebfd416b81
+size 557
